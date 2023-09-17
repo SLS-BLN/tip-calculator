70096fdcde16db033a25e5131010074414cb8c8a
Stefan Schlicker <stefanschlicker@posteo.de>
2023-09-17 09:58:15 +0200

    remove cdn, only dependencies and imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-// import "fundamental-styles/dist/theming/sap_horizon.css";
-// import "@sap-theming/theming-base-content/content/Base/baseLib/sap_horizon/css_variables.css";
+import "fundamental-styles/dist/theming/sap_horizon_dark.css";
+import "@sap-theming/theming-base-content/content/Base/baseLib/sap_horizon_dark/css_variables.css";
 
-// import "fundamental-styles/dist/button.css";
-// import "fundamental-styles/dist/avatar.css";
-// import "fundamental-styles/dist/input.css";
-// import "fundamental-styles/dist/menu.css";
-// import "fundamental-styles/dist/bar.css";
-// import "fundamental-styles/dist/page.css";
-// import "fundamental-styles/dist/icon.css";
-// import "fundamental-styles/dist/popover.css";
-// import "fundamental-styles/dist/shellbar.css";
+import "fundamental-styles/dist/avatar.css";
+import "fundamental-styles/dist/bar.css";
+import "fundamental-styles/dist/button.css";
+import "fundamental-styles/dist/dynamic-page.css";
+import "fundamental-styles/dist/fundamental-styles.css";
+import "fundamental-styles/dist/icon.css";
+import "fundamental-styles/dist/input.css";
+import "fundamental-styles/dist/menu.css";
+import "fundamental-styles/dist/page.css";
+import "fundamental-styles/dist/popover.css";
+import "fundamental-styles/dist/section.css";
+import "fundamental-styles/dist/shellbar.css";
+import "fundamental-styles/dist/title.css";
+import "fundamental-styles/dist/toolbar.css";
 
 import "./index.css";
 
