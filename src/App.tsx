07b1968de4b71892636cb1b436c71b17901b821e
Stefan Schlicker <stefanschlicker@posteo.de>
2023-09-24 10:19:55 +0200

    add theming script

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import DiningBill from "./components/DiningBill";
 import ServiceRating from "./components/ServiceRating";
 import Total from "./components/Total";
 import Reset from "./components/Reset";
+
+import "@ui5/webcomponents-react/dist/Assets";
 import {
   Avatar,
   Bar,
@@ -68,12 +70,10 @@ function App() {
             />
           </Bar>
         }
-        backgroundDesign="Transparent"
         style={{ height: "90vh" }}
       >
         <Form>
           <DiningBill onBill={setBill} bill={bill} />
-
           <FormGroup titleText="Rate the Service">
             <FormItem label="How did your like the service">
               <ServiceRating
@@ -97,74 +97,3 @@ function App() {
 }
 
 export default App;
-
-{
-  /* <DynamicPage
-  headerContent={
-    <DynamicPageHeader>
-      <FlexBox wrap="Wrap">
-        <FlexBox direction="Column">
-          <Label>Location: Warehouse A</Label>
-          <Label>Halway: 23L</Label>
-          <Label>Rack: 34</Label>
-        </FlexBox>
-        <span style={{ width: "1rem" }} />
-        <FlexBox direction="Column">
-          <Label>Availability:</Label>
-          <ObjectStatus state="Success">In Stock</ObjectStatus>
-        </FlexBox>
-      </FlexBox>
-    </DynamicPageHeader>
-  }
-  headerTitle={
-    <DynamicPageTitle
-      actions={
-        <>
-          <Button design="Emphasized">Edit</Button>
-          <Button design="Transparent">Delete</Button>
-          <Button design="Transparent">Copy</Button>
-          <Button design="Transparent" icon="action" />
-        </>
-      }
-      breadcrumbs={
-        <Breadcrumbs>
-          <BreadcrumbsItem>Home</BreadcrumbsItem>
-          <BreadcrumbsItem>Page 1</BreadcrumbsItem>
-          <BreadcrumbsItem>Page 2</BreadcrumbsItem>
-          <BreadcrumbsItem>Page 3</BreadcrumbsItem>
-          <BreadcrumbsItem>Page 4</BreadcrumbsItem>
-          <BreadcrumbsItem>Page 5</BreadcrumbsItem>
-        </Breadcrumbs>
-      }
-      expandedContent={
-        <MessageStrip>
-          Information (only visible if header content is expanded)
-        </MessageStrip>
-      }
-      header={<Title>Header Title</Title>}
-      navigationActions={
-        <>
-          <Button design="Transparent" icon="full-screen" />
-          <Button design="Transparent" icon="exit-full-screen" />
-          <Button design="Transparent" icon="decline" />
-        </>
-      }
-      snappedContent={
-        <MessageStrip>
-          Information (only visible if header content is collapsed/snapped)
-        </MessageStrip>
-      }
-      subHeader={<Label>This is a sub header</Label>}
-    >
-      <Badge>Status: OK</Badge>
-    </DynamicPageTitle>
-  }
-  onPinnedStateChange={function ka() {}}
-  onToggleHeaderContent={function ka() {}}
-  style={{
-    maxHeight: "700px",
-  }}
->
-  <M />
-</DynamicPage>; */
-}
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import { ThemeProvider } from "@ui5/webcomponents-react";
 import App from "./App.tsx";
 import "./index.css";
 
+// import "@ui5/webcomponents/dist/Assets.js";
+// import "@ui5/webcomponents-react/dist/Assets.js";
+// import "@ui5/webcomponents-icons/dist/Assets.js";
+// import "@ui5/webcomponents-fiori/dist/Assets.js";
+
+// import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
+// setTheme("sap_fiori_3_dark");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
